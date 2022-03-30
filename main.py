84aefc85283f283d7962378345a71f10dac833d6
IvanJairus <filemonivanjairus@gmail.com>
2022-03-30 13:59:38 +0700

    Update main.py

--- main.py
+++ main.py
@@ -79,11 +79,11 @@ def user(id):
         conn.close()
 
 
-@app.route('/update', methods=['POST'])
-def update_user():
+@app.route('/update/<int:id>', methods=['POST'])
+def update_user(id):
     try:
         _json = request.json
-        _id = _json['id']
+        # _id = _json['user_id']
         _nama = _json['nama']
         _alamat = _json['alamat']
         _tempat_lahir = _json['tempat_lahir']
@@ -91,11 +91,10 @@ def update_user():
         _no_KTP = _json['no_KTP']
         _no_HP = _json['no_HP']
         # validate the received values
-        if _nama and _alamat and _tempat_lahir and _id and _tanggal_lahir and _no_KTP and _no_HP and request.method == 'POST':
-            # do not save password as a plain text
-            # save edits
-            sql = "UPDATE tbl_karyawan SET nama=%s, alamat=%s, tempat_lahir=%s, tanggal_lahir=%s, no_KTP=%s, no_HP=%s WHERE user_id=%s"
-            data = (_nama, _alamat, _tempat_lahir, _id, _tanggal_lahir, _no_KTP, _no_HP,)
+        if _nama and _alamat and _tempat_lahir and _tanggal_lahir and _no_KTP and _no_HP and request.method == 'POST':
+         
+            sql = "UPDATE tbl_karyawan SET nama=%s, alamat=%s, tempat_lahir=%s, tanggal_lahir=%s, no_KTP=%s, no_HP=%s WHERE no_KTP=%s"
+            data = (_nama, _alamat, _tempat_lahir, _tanggal_lahir, _no_KTP, _no_HP, id)
             conn = mysql.connect()
             cursor = conn.cursor()
             cursor.execute(sql, data)
@@ -112,12 +111,12 @@ def update_user():
         conn.close()
 
 
-@app.route('/delete/<int:id>')
+@app.route('/delete/<int:id>') #GET
 def delete_user(id):
     try:
         conn = mysql.connect()
         cursor = conn.cursor()
-        cursor.execute("DELETE FROM tbl_karyawan WHERE user_id=%s", (id,))
+        cursor.execute("DELETE FROM tbl_karyawan WHERE no_KTP=%s", (id,))
         conn.commit()
         resp = jsonify('User deleted successfully!')
         resp.status_code = 200
@@ -144,11 +143,15 @@ def handle_exception(e):
     """Return JSON instead of HTML for HTTP errors."""
     # start with the correct headers and status code from the error
     response = e.get_response()
+    if e.description == "The server encountered an internal error and was unable to complete your request. Either the server is overloaded or there is an error in the application.":
+        res = "No. KTP Sudah Ada",
+    else:
+        res = e.description,
     # replace the body with JSON
     response.data = json.dumps({
         "code": e.code,
         # "name": e.name,
-        "description": 'No. KTP sudah ada.',
+        "description": res
     })
     response.content_type = "application/json"
     return response
